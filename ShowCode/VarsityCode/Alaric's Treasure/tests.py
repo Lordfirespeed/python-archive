7310c3c95913487ae1e1f1164c6b5ecbb0ddc0b1
Lordfirespeed <joe@joeclack.me.uk>
2022-10-10 22:30:18 +0100

    dynamically populate tests since they are all of the same form

--- ShowCode/VarsityCode/Alaric's Treasure/tests.py
+++ ShowCode/VarsityCode/Alaric's Treasure/tests.py
@@ -3,11 +3,32 @@ import unittest
 
 
 class TreasureHunterTests(unittest.TestCase):
+    numeral_tests = {
+        "MCMLXI": 1961,
+        "MMXV": 2015,
+        "CCCIX": 309,
+        "CCLXXXVI": 286,
+        "LIV": 54,
+        "XX": 20,
+        "DXX": 520,
+        "CCLXVII": 267,
+        "CXXXV": 135,
+        "CDLIV": 454,
+        "DXXXVI": 536,
+        "DCCLII": 752
+    }
 
-    def test1(self):
+    @classmethod
+    def populate(cls):
+        for numeral, expected_value in cls.numeral_tests.items():
+            setattr(cls, f"test_{numeral}", lambda self: self.generic_numeral_test(numeral, expected_value))
+
+    def generic_numeral_test(self, numerals: str, expected: int) -> None:
         result = TreasureHunter()
-        self.assertEqual(result.parse_roman_numerals("MCMLXI"), 1961)
+        self.assertEqual(expected, result.parse_roman_numerals(numerals))
 
+
+TreasureHunterTests.populate()
 
 if __name__ == '__main__':
     unittest.main()
