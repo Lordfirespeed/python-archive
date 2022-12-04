5677f330fe741e2eca5c843195a4634a58a08262
Lordfirespeed <joe@joeclack.me.uk>
2022-12-04 18:45:39 +0000

    complete day 3 part one

--- Advent of Code/2022/day_3/part_one.py
+++ Advent of Code/2022/day_3/part_one.py
@@ -1,15 +1,45 @@
-class Solution:
-    def __init__(self, input_lines):
-        self.input_lines = input_lines
+from string import ascii_lowercase, ascii_uppercase
 
-    def foo(self):
-        return len(self.input_lines)
+
+class Backpack:
+    def __init__(self, backpack_string: str) -> None:
+        midpoint_index = len(backpack_string) // 2
+        self.first_compartment = set(backpack_string[:midpoint_index])
+        self.second_compartment = set(backpack_string[midpoint_index:])
+
+    def item_in_both_compartments(self) -> str:
+        intersect = self.first_compartment.intersection(self.second_compartment)
+        return intersect.pop()
+
+
+class Solution:
+    def __init__(self, backpacks: [str]) -> None:
+        self.backpack_strings = backpacks
+        self.backpacks = map(Backpack, self.backpack_strings)
+
+    @staticmethod
+    def score_item(item: str) -> int:
+        try:
+            return ascii_lowercase.index(item) + 1
+        except ValueError:
+            pass
+
+        try:
+            return ascii_uppercase.index(item) + 27
+        except ValueError:
+            pass
+
+        raise ValueError
+
+
+    def priority_sum(self):
+        return sum([self.score_item(backpack.item_in_both_compartments()) for backpack in self.backpacks])
 
 
 if __name__ == "__main__":
     with open(r"input.txt") as input_file:
-        input_lines = [line.strip() for line in input_file.readlines()]
+        backpacks = [line.strip() for line in input_file.readlines()]
 
-    solver = Solution(input_lines)
-    result = solver.foo()
-    print(f"foo: {result}")
+    solver = Solution(backpacks)
+    result = solver.priority_sum()
+    print(f"Sum of priorities of doubly-placed items: {result}")
