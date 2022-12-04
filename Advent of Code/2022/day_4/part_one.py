a5ae42e30ef8eea27932663350c90d7a517e564b
Lordfirespeed <joe@joeclack.me.uk>
2022-12-04 19:15:26 +0000

    complete day 4 part one

--- Advent of Code/2022/day_4/part_one.py
+++ Advent of Code/2022/day_4/part_one.py
@@ -1,9 +1,34 @@
-class Solution:
-    def __init__(self, input_lines):
-        self.input_lines = input_lines
+class ElfPair:
+    def __init__(self, assignment_string: str) -> None:
+        first_range_string, second_range_string = assignment_string.split(",")
+        self.first_range = self.range_from_range_string(first_range_string)
+        self.second_range = self.range_from_range_string(second_range_string)
 
-    def foo(self):
-        return len(self.input_lines)
+    @staticmethod
+    def range_from_range_string(range_string: str) -> slice:
+        start_string, end_string = range_string.split("-")
+        return slice(int(start_string), int(end_string))
+
+    @staticmethod
+    def a_contains_b(a: slice, b: slice) -> bool:
+        return a.start <= b.start and a.stop >= b.stop
+
+    def first_contains_second(self) -> bool:
+        return self.a_contains_b(self.first_range, self.second_range)
+
+    def second_contains_first(self) -> bool:
+        return self.a_contains_b(self.second_range, self.first_range)
+
+    def assignment_fully_overlaps(self) -> bool:
+        return self.first_contains_second() or self.second_contains_first()
+
+
+class Solution:
+    def __init__(self, elf_assignments: [str]) -> None:
+        self.elf_pairs = list(map(ElfPair, elf_assignments))
+
+    def count_full_overlaps(self):
+        return [pair.assignment_fully_overlaps() for pair in self.elf_pairs].count(True)
 
 
 if __name__ == "__main__":
@@ -11,5 +36,5 @@ if __name__ == "__main__":
         input_lines = [line.strip() for line in input_file.readlines()]
 
     solver = Solution(input_lines)
-    result = solver.foo()
-    print(f"foo: {result}")
+    result = solver.count_full_overlaps()
+    print(f"Number of assignment pairs with complete overlaps: {result}")
