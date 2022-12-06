19e36ad7a40ccd68838996f8297d7e44247adfd1
Lordfirespeed <joe@joeclack.me.uk>
2022-12-06 18:07:47 +0000

    complete part one of day 6

--- Advent of Code/2022/day_6/part_one.py
+++ Advent of Code/2022/day_6/part_one.py
@@ -1,15 +1,20 @@
 class Solution:
-    def __init__(self, input_lines: [str]) -> None:
-        self.input_lines = input_lines
+    target_length = 4
+    def __init__(self, datastream: str) -> None:
+        self.datastream = datastream
 
-    def foo(self) -> int:
-        return len(self.input_lines)
+    def index_of_first_marker(self) -> int:
+        """The 'naive' solution - will not work for large target_length"""
+        for current_index in range(len(self.datastream) - self.target_length):
+            datastream_slice = datastream[current_index:current_index + self.target_length]
+            if len(set(datastream_slice)) == self.target_length:
+                return current_index + self.target_length
 
 
 if __name__ == "__main__":
     with open(r"input.txt") as input_file:
-        input_lines = [line.strip() for line in input_file.readlines()]
+        datastream = input_file.readline()
 
-    solver = Solution(input_lines)
-    result = solver.foo()
-    print(f"foo: {result}")
+    solver = Solution(datastream)
+    result = solver.index_of_first_marker()
+    print(f"First packet marker found after {result} processed characters.")
