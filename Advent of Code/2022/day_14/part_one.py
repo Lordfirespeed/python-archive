fbf634d6928d19d25c3672029e5794ad31ef0bdc
Joseph Clack <joe@joeclack.me.uk>
2022-12-14 18:09:00 +0000

    amend part one solution: move methods for generating walls into the Cave class. Will make part two easier to implement

--- Advent of Code/2022/day_14/part_one.py
+++ Advent of Code/2022/day_14/part_one.py
@@ -16,8 +16,40 @@ class SandInVoid(Exception):
 
 
 class Cave:
-    def __init__(self):
+    def __init__(self, wall_strings: [str]):
         self.tiles: defaultdict[int, dict[int, TileState]] = defaultdict(dict)
+        self.generate_walls(wall_strings)
+        self.maximum_height_wall = max(self.tiles.keys())
+
+    @staticmethod
+    def line_of_vectors(start: Vector2, end: Vector2) -> [Vector2]:
+        if start.x == end.x:
+            if start.y > end.y:
+                start, end = end, start
+
+            return [Vector2(start.x, y) for y in range(start.y, end.y + 1)]
+
+        if start.y == end.y:
+            if start.x > end.x:
+                start, end = end, start
+
+            return [Vector2(x, start.y) for x in range(start.x, end.x + 1)]
+
+        raise ValueError
+
+    def generate_one_wall_segment(self, start: Vector2, end: Vector2) -> None:
+        for position in self.line_of_vectors(start, end):
+            self[position] = TileState.Rock
+
+    def generate_one_wall(self, wall_string: str) -> None:
+        string_vertex_positions = [vertex.split(",") for vertex in wall_string.split(" -> ")]
+        vector_vertex_positions = [Vector2(int(x), int(y)) for x, y in string_vertex_positions]
+        for vector_pair in zip(vector_vertex_positions[:-1], vector_vertex_positions[1:]):
+            self.generate_one_wall_segment(*vector_pair)
+
+    def generate_walls(self, wall_strings: [str]) -> None:
+        for wall_string in wall_strings:
+            self.generate_one_wall(wall_string)
 
     @overload
     def __getitem__(self, item: tuple[int, int] | Vector2) -> TileState: ...
@@ -56,41 +88,8 @@ class Solution:
     sand_fall_deltas = (Vector2(0, 1), Vector2(-1, 1), Vector2(1, 1))
 
     def __init__(self, input_lines: [str]) -> None:
-        self.wall_strings = input_lines
-        self.cave = Cave()
-        self.generate_cave_walls()
-        self.maximum_height_wall = max(self.cave.tiles.keys())
+        self.cave = Cave(input_lines)
         self.number_sand_at_rest = 0
-
-    @staticmethod
-    def line_of_vectors(start: Vector2, end: Vector2) -> [Vector2]:
-        if start.x == end.x:
-            if start.y > end.y:
-                start, end = end, start
-
-            return [Vector2(start.x, y) for y in range(start.y, end.y+1)]
-
-        if start.y == end.y:
-            if start.x > end.x:
-                start, end = end, start
-
-            return [Vector2(x, start.y) for x in range(start.x, end.x+1)]
-
-        raise ValueError
-
-    def generate_cave_wall_segment(self, start: Vector2, end: Vector2) -> None:
-        for position in self.line_of_vectors(start, end):
-            self.cave[position] = TileState.Rock
-
-    def generate_cave_wall(self, wall_string: str) -> None:
-        string_vertex_positions = [vertex.split(",") for vertex in wall_string.split(" -> ")]
-        vector_vertex_positions = [Vector2(int(x), int(y)) for x, y in string_vertex_positions]
-        for vector_pair in zip(vector_vertex_positions[:-1], vector_vertex_positions[1:]):
-            self.generate_cave_wall_segment(*vector_pair)
-
-    def generate_cave_walls(self) -> None:
-        for wall_string in self.wall_strings:
-            self.generate_cave_wall(wall_string)
 
     def spawn_sand(self) -> None:
         current_sand_position = copy(self.sand_source)
@@ -105,13 +104,12 @@ class Solution:
                 break
             else:
                 break
-            if current_sand_position.y > self.maximum_height_wall:
+            if current_sand_position.y > self.cave.maximum_height_wall:
                 raise SandInVoid
         self.number_sand_at_rest += 1
         self.cave[current_sand_position] = TileState.Sand
 
     def run_simulation(self):
-        self.generate_cave_walls()
         while True:
             try:
                 self.spawn_sand()
