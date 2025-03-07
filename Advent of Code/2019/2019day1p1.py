7c025db4b6920bccff6222857085f4116bf11185
Lordfirespeed <28568841+Lordfirespeed@users.noreply.github.com>
2025-03-07 13:37:22 +0000

    remove advent of code sources; they have been moved to https://github.com/Lordfirespeed/advent-of-code

--- Advent of Code/2019/2019day1p1.py
+++
@@ -1,5 +0,0 @@
-with open("Input/2019day1input.txt") as inputfile:
-    nums = [int(line.strip()) for line in inputfile.readlines()]
-
-req = [n // 3 - 2 for n in nums]
-print(sum(req))
--- Advent of Code/2022/day_13/experiment.py
+++
@@ -1,6 +0,0 @@
-from part_one import PacketParser, Solution
-
-with open("expected_test_output.txt") as expected_output_file:
-    packets = [PacketParser.parse_string(line) for line in expected_output_file]
-
-solver = Solution([])
