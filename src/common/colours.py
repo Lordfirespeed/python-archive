da463f5cf32b6db22b98bd28a29bdc37d5afdc44
Lordfirespeed <28568841+Lordfirespeed@users.noreply.github.com>
2025-03-12 14:08:28 +0000

    little clarification

--- src/common/colours.py
+++ src/common/colours.py
@@ -1,2 +1,7 @@
-pink = "\033[95m"
-reset = "\033[0m"
+"""
+See https://gist.github.com/fnky/458719343aabd01cfb17a3a4f7296797
+Note 0x1b = 27, the ASCII character ordinal for 'Escape'.
+"""
+
+pink = "\x1b[95m"
+reset = "\x1b[0m"
